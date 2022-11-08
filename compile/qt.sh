7b050e7ceb5a674d864fac11faf2877a9b532691
IamLupo <help0001@gmail.com>
2022-11-08 04:22:21 +0100

    add: 3 new patches for qt 5.15.7.

--- compile/qt.sh
+++ compile/qt.sh
@@ -9,11 +9,13 @@ cd qt-everywhere-src-5.15.7
 cp ../../../../patch/qfreetypefontdatabase_p.h qtbase/include/QtFontDatabaseSupport/5.15.7/QtFontDatabaseSupport/private
 cp ../../../../patch/qwindowsfontdatabase_ft_p.h qtbase/include/QtFontDatabaseSupport/5.15.7/QtFontDatabaseSupport/private
 cp ../../../../patch/qwindowsfontdatabase_p.h qtbase/include/QtFontDatabaseSupport/5.15.7/QtFontDatabaseSupport/private
+cp ../../../../patch/qfontengine_ft_p.h qtbase/include/QtFontDatabaseSupport/5.15.7/QtFontDatabaseSupport/private
+cp ../../../../patch/qwindowsnativeimage_p.h qtbase/include/QtFontDatabaseSupport/5.15.7/QtFontDatabaseSupport/private
+cp ../../../../patch/qwindowsfontengine_p.h qtbase/include/QtFontDatabaseSupport/5.15.7/QtFontDatabaseSupport/private
 
 cp ../../../../patch/qwindowsguieventdispatcher_p.h qtbase/include/QtEventDispatcherSupport/5.15.7/QtEventDispatcherSupport/private
 
 cp ../../../../patch/qwindowsuiawrapper_p.h qtbase/include/QtWindowsUIAutomationSupport/5.15.7/QtWindowsUIAutomationSupport/private
-
 
 ./configure -static -release -opensource -confirm-license -prefix "$PWD/../../libs/qt-5.15.7" -qt-zlib -qt-libpng -qt-webp -qt-libjpeg -qt-freetype  -no-opengl -skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcharts -skip qtconnectivity -skip qtdatavis3d -skip qtdeclarative -skip qtdoc -skip qtgamepad -skip qtlocation -skip qtlottie -skip qtmacextras -skip qtmultimedia -skip qtnetworkauth -skip qtpurchasing -skip qtquick3d -skip qtquickcontrols -skip qtquickcontrols2 -skip qtquicktimeline -skip qtremoteobjects -skip qtscript -skip qtsensors -skip qtspeech -skip qtsvg -skip qtwayland -skip qtwebglplugin -skip qtwebview -skip webengine -make libs -nomake tools -nomake examples -nomake tests $1
 make -j 8
