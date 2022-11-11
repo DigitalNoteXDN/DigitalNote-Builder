85f9af9b888c71c9a6ec56d7ba52004c8b673947
IamLupo <help0001@gmail.com>
2022-11-11 01:30:16 +0100

    patch: mac os berkeley downloaded patch in project.

--- macos/compile/berkeleydb.sh
+++ macos/compile/berkeleydb.sh
@@ -6,8 +6,7 @@ tar xvfz ../../../download/db-6.2.32.NC.tar.gz
 
 cd db-6.2.32.NC
 
-curl -OL https://raw.githubusercontent.com/macports/macports-ports/cb92cb90bdc7fb90212e928db32172546eca0f5b/databases/db60/files/patch-src_dbinc_atomic.h
-patch -s -p0 < patch-src_dbinc_atomic.h
+patch -s -p0 < ../../../patch/patch-src_dbinc_atomic.h
 
 cd build_unix
 
