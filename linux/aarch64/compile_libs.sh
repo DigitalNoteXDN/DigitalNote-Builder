8c928d2bb3b6d91280a3f89e46d865468ab89679
IamLupo <help0001@gmail.com>
2022-11-25 18:40:45 +0100

    fix: aarch64 host parameter

--- linux/aarch64/compile_libs.sh
+++ linux/aarch64/compile_libs.sh
@@ -17,5 +17,5 @@ echo 'using gcc : aarch64 : aarch64-linux-gnu-g++ ;' > config/user-config.jam
 ../../compile/miniupnpc.sh "libminiupnpc.a" $1
 ../../compile/openssl.sh "linux-aarch64" $1
 ../../compile/qrencode.sh  "--host aarch64-linux-gnu" $1
-../../compile/secp256k1.sh "--host=i686-linux-gnu" $1
+../../compile/secp256k1.sh "--host aarch64-linux-gnu" $1
 ../../compile/qt.sh "-platform linux-g++ -xplatform linux-aarch64-gnu-g++" $1
