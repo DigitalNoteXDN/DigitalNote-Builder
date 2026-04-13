4715bdbab2acc57e9b6c096666dc4f184c2a2984
Rubber-Duckie <53319864+rubber-duckie-au@users.noreply.github.com>
2026-04-13 19:19:34 +1000

    Lib Source files update
    Update URLs for lib sources
    added autogen.sh for libqrencode

--- compile/qrencode.sh
+++ compile/qrencode.sh
@@ -2,9 +2,11 @@
 
 cd temp
 
-tar xvfz ../../../download/qrencode-4.1.1.tar.gz
+tar xvfz ../../../download/libqrencode-4.1.1.tar.gz
 
-cd qrencode-4.1.1
+cd libqrencode-4.1.1
+
+./autogen.sh
 
 ./configure --enable-static --disable-shared --without-tools --prefix=$PWD/../../libs/qrencode-4.1.1 $1
 make $2
